477fae1000e96a54c2fea58daa2f0387e26e50d2
Berk Disli <berkdisli@yahoo.com>
2022-04-07 14:03:59 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import Metronome from './Metronome';
 
-const express = require("express");
 
 ReactDOM.render(
   <Metronome />, document.getElementById('root')
 );
 
-App.use(express.static(__dirname + "/docs"))
